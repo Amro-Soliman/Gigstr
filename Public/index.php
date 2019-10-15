b4e62ba107c81e69eb6e85a282bcda2267518d9a
amro-soliman <amro.soliman@protonmail.com>
2019-10-15 15:09:56 +0200

    first commit

---
+++ Public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow&display=swap" rel="stylesheet">  
+<link href="../Assets/main.css" rel="stylesheet">
+<title>Index</title>
+</head>
+<body>
+<div id='container'>
+
+<h4 id='logo'>Gigstr <span>BETA<span></h4>
+<ul id='nav'>
+
+<li> <a href="#">utförska</a></li>
+<li> <a href="#">Offert & Faktura</a></li>
+<li> <a href="#">Personal</a></li>
+<li> <a href="#">Rapporter</a></li>
+<li> <a href="#">Support</a></li>
+</ul>
+<div id='main'>
+
+</div>
+</div>
